3956ff607a65ee7c374aee7abd9364c9ab718191
vipinsharma0999 <136472185+vipinsharma0999@users.noreply.github.com>
2024-08-09 12:25:24 +0530

    changes

--- src/components/Tables/TableMui/TS.tsx
+++ src/components/Tables/TableMui/TS.tsx
@@ -6,18 +6,11 @@ import {
   MaterialReactTable,
   useMaterialReactTable,
   type MRT_ColumnDef,
-  MRT_GlobalFilterTextField,
-  MRT_ToggleFiltersButton,
 } from 'material-react-table';
 
 //Material UI Imports
 import {
   Box,
-  Button,
-  ListItemIcon,
-  MenuItem,
-  Typography,
-  lighten,
 } from '@mui/material';
 
 //Icons Imports
@@ -43,7 +36,7 @@ const Example = () => {
     () => [
       {
         id: 'employee', //id used to define `group` column
-        header: 'Employee',
+        header: 'Employee',  // It is the header employee
         columns: [
           {
             accessorFn: (row) => `${row.firstName} ${row.lastName}`, //accessorFn used to join multiple data into a single cell
@@ -58,14 +51,6 @@ const Example = () => {
                   gap: '1rem',
                 }}
               >
-                <img
-                  alt="avatar"
-                  height={30}
-                  src={row.original.avatar}
-                  loading="lazy"
-                  style={{ borderRadius: '50%' }}
-                />
-                {/* using renderedCellValue instead of cell.getValue() preserves filter match highlighting */}
                 <span>{renderedCellValue}</span>
               </Box>
             ),
@@ -85,7 +70,7 @@ const Example = () => {
         columns: [
           {
             accessorKey: 'salary',
-            // filterVariant: 'range', //if not using filter modes feature, use this instead of filterFn
+            filterVariant: 'range', //if not using filter modes feature, use this instead of filterFn
             filterFn: 'between',
             header: 'Salary',
             size: 200,
@@ -121,21 +106,21 @@ const Example = () => {
             header: 'Job Title',
             size: 350,
           },
-          {
-            accessorFn: (row) => new Date(row.startDate), //convert to Date for sorting and filtering
-            id: 'startDate',
-            header: 'Start Date',
-            filterVariant: 'date',
-            filterFn: 'lessThan',
-            sortingFn: 'datetime',
-            Cell: ({ cell }) => cell.getValue<Date>()?.toLocaleDateString(), //render Date as a string
-            Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
-            muiFilterTextFieldProps: {
-              sx: {
-                minWidth: '250px',
-              },
-            },
-          },
+          // {
+          //   accessorFn: (row) => new Date(row.startDate), //convert to Date for sorting and filtering
+          //   id: 'startDate',
+          //   header: 'Start Date',
+          //   filterVariant: 'date',
+          //   filterFn: 'lessThan',
+          //   sortingFn: 'datetime',
+          //   Cell: ({ cell }) => cell.getValue<Date>()?.toLocaleDateString(), //render Date as a string
+          //   Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
+          //   muiFilterTextFieldProps: {
+          //     sx: {
+          //       minWidth: '250px',
+          //     },
+          //   },
+          // },
         ],
       },
     ],
@@ -150,8 +135,6 @@ const Example = () => {
     enableGrouping: true,
     enableColumnPinning: true,
     enableFacetedValues: true,
-    enableRowActions: true,
-    enableRowSelection: true,
     initialState: {
       showColumnFilters: true,
       showGlobalFilter: true,
@@ -171,126 +154,6 @@ const Example = () => {
       rowsPerPageOptions: [10, 20, 30],
       shape: 'rounded',
       variant: 'outlined',
-    },
-    renderDetailPanel: ({ row }) => (
-      <Box
-        sx={{
-          alignItems: 'center',
-          display: 'flex',
-          justifyContent: 'space-around',
-          left: '30px',
-          maxWidth: '1000px',
-          position: 'sticky',
-          width: '100%',
-        }}
-      >
-        <img
-          alt="avatar"
-          height={200}
-          src={row.original.avatar}
-          loading="lazy"
-          style={{ borderRadius: '50%' }}
-        />
-        <Box sx={{ textAlign: 'center' }}>
-          <Typography variant="h4">Signature Catch Phrase:</Typography>
-          <Typography variant="h1">
-            &quot;{row.original.signatureCatchPhrase}&quot;
-          </Typography>
-        </Box>
-      </Box>
-    ),
-    renderRowActionMenuItems: ({ closeMenu }) => [
-      <MenuItem
-        key={0}
-        onClick={() => {
-          // View profile logic...
-          closeMenu();
-        }}
-        sx={{ m: 0 }}
-      >
-        <ListItemIcon>
-          <AccountCircle />
-        </ListItemIcon>
-        View Profile
-      </MenuItem>,
-      <MenuItem
-        key={1}
-        onClick={() => {
-          // Send email logic...
-          closeMenu();
-        }}
-        sx={{ m: 0 }}
-      >
-        <ListItemIcon>
-          <Send />
-        </ListItemIcon>
-        Send Email
-      </MenuItem>,
-    ],
-    renderTopToolbar: ({ table }) => {
-      const handleDeactivate = () => {
-        table.getSelectedRowModel().flatRows.map((row) => {
-          alert('deactivating ' + row.getValue('name'));
-        });
-      };
-
-      const handleActivate = () => {
-        table.getSelectedRowModel().flatRows.map((row) => {
-          alert('activating ' + row.getValue('name'));
-        });
-      };
-
-      const handleContact = () => {
-        table.getSelectedRowModel().flatRows.map((row) => {
-          alert('contact ' + row.getValue('name'));
-        });
-      };
-
-      return (
-        <Box
-          sx={(theme) => ({
-            backgroundColor: lighten(theme.palette.background.default, 0.05),
-            display: 'flex',
-            gap: '0.5rem',
-            p: '8px',
-            justifyContent: 'space-between',
-          })}
-        >
-          <Box sx={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}>
-            {/* import MRT sub-components */}
-            <MRT_GlobalFilterTextField table={table} />
-            <MRT_ToggleFiltersButton table={table} />
-          </Box>
-          <Box>
-            <Box sx={{ display: 'flex', gap: '0.5rem' }}>
-              <Button
-                color="error"
-                disabled={!table.getIsSomeRowsSelected()}
-                onClick={handleDeactivate}
-                variant="contained"
-              >
-                Deactivate
-              </Button>
-              <Button
-                color="success"
-                disabled={!table.getIsSomeRowsSelected()}
-                onClick={handleActivate}
-                variant="contained"
-              >
-                Activate
-              </Button>
-              <Button
-                color="info"
-                disabled={!table.getIsSomeRowsSelected()}
-                onClick={handleContact}
-                variant="contained"
-              >
-                Contact
-              </Button>
-            </Box>
-          </Box>
-        </Box>
-      );
     },
   });
 
